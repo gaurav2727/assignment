b0b7961a8ad7eba476274bbbea47e7f233638b5a
Gaurav Bansal <gauravbansal1024@gmail.com>
2024-07-01 22:27:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import SortableList from './SortableList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sortable List</h1>
+      <SortableList />
+    </div>
+  );
+}
+
+export default App;
